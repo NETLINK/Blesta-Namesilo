022c66e3350099e84a38c28f7125f3a736e503b6
Abdy Franco <yosoy@abdyfran.co>
2020-07-01 09:17:52 -0600

    Remove getEmailTags() method

--- namesilo.php
+++ namesilo.php
@@ -1071,26 +1071,6 @@ class Namesilo extends Module
     }
 
     /**
-     * Returns an array of key values for fields stored for a module, package,
-     * and service under this module, used to substitute those keys with their
-     * actual module, package, or service meta values in related emails.
-     *
-     * @return array A multi-dimensional array of key/value pairs where each key is one of 'module', 'package',
-     *  or 'service' and each value is a numerically indexed array of key values that match meta fields under
-     *  that category.
-     * @see Modules::addModuleRow()
-     * @see Modules::editModuleRow()
-     * @see Modules::addPackage()
-     * @see Modules::editPackage()
-     * @see Modules::addService()
-     * @see Modules::editService()
-     */
-    public function getEmailTags()
-    {
-        return ['service' => ['domain']];
-    }
-
-    /**
      * Returns all fields to display to an admin attempting to add a service with the module
      *
      * @param stdClass $package A stdClass object representing the selected package
